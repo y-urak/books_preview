cd94de1e20095c686e485ad16a9c6f04e8300f8f
y-urak <yumau717@ezweb.ne.jp>
2022-06-11 15:37:03 +0900

    bookの表示部分を1/2にしてそれ以外の領域をカテゴリの表示として利用する

--- book.js
+++ book.js
@@ -1,12 +1,26 @@
 class book{
+    /*
+    constructor(w,h,x,y){
+        this.w=w
+        this.h=h
+        this.x=x
+        this.y=y
+    }
+    */
     constructor(w,h,x,y,url){
         this.w=w
         this.h=h
         this.x=x
         this.y=y
-        console.log(typeof(createImg(url)))
+        //console.log(typeof(createImg(url)))
         this.img = createImg(url);
     }
+    showNonImage(){
+        colorMode(RGB);
+        fill(255, 204, 100);
+        rect(this.x,this.y,this.w,this.h);
+    }
+
     show(){
         rect(this.x,this.y,this.w,this.h);
         let image_x=this.x+this.w*1/6
--- sketch.js
+++ sketch.js
@@ -11,21 +11,34 @@ function preload(){
 function setup() {
   names = sheet_data.getColumn('書名');
   urls = sheet_data.getColumn('URL');
-  console.log(names);
-  console.log(urls);
+  //console.log(names);
+  //console.log(urls);
   createCanvas(1900, 1000);
   num = 5
   x_interval = width/num
-  y_interval = height/10 * 9
+  y_interval = height/2 -20
   for(let i=0;i<num;i++){
-    b= new book(x_interval-50,y_interval, x_interval*i,10,'https://elib.maruzen.co.jp/app/images/l/2/3000072732.jpg')
-    books.push(b)
+    //b= new book(x_interval-50,y_interval, x_interval*i,height/4+10,'https://elib.maruzen.co.jp/app/images/l/2/3000072732.jpg')
+    //
+    b= new book(x_interval-50,y_interval, x_interval*i,height/4+10);
+    books.push(b);
   }
 }
 
 function draw() {
   background(220);
   for(let i=0;i<books.length;i++){
-    books[i].show()
+    books[i].showNonImage();
+    //books[i].show();
   }
+  colorMode(HSB);
+  fill(255, 204, 100);
+  rect(10,500,200,300);
+  line(0,height/2,width,50);
+  //line(0,height/8,width,height/8);
+  line(0,height/12,width,height/12);
+  line(0,height*2/12,width,height*2/12);
+  line(0,height/4,width,height/4);
+  line(0,height*3/4,width,height*3/4);
+  line(0,height*7/8,width,height*7/8);
 }
